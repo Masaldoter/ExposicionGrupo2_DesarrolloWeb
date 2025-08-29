485a8f6f1561407a307c24bf79dd5970782a4418
Masaldoter <aldoyagu@gmail.com>
2025-08-28 23:34:19 -0600

    Complementos para el CRUD, nuevo diseño y cambio de Logica

--- backend/pedidos/urls.py
+++ backend/pedidos/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.lista_pedidos, name='lista_pedidos'),
     path('crear/', views.crear_pedido, name='crear_pedido'),
+    path('editar/<int:id>/', views.editar_pedido, name='editar_pedido'),
+    path('cancelar/<int:id>/', views.cancelar_pedido, name='cancelar_pedido'),
+    path('entregar/<int:id>/', views.entregar_pedido, name='entregar_pedido'),
 ]
